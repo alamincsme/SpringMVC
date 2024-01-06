e6e3d854475c9e6dac214f26e3b0f4fd3f1d58dd
alamincsme <alamincsme@gmail.com>
2024-01-06 06:58:10 +0600

    Spring model and view

--- src/main/java/com/alamincsme/springboot/controller/MainController.java
+++ src/main/java/com/alamincsme/springboot/controller/MainController.java
@@ -1,8 +1,13 @@
 package com.alamincsme.springboot.controller;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class MainController {
@@ -10,5 +15,14 @@ public class MainController {
 	@GetMapping("/home")
 	public String homePage() {
 		return "home.html";
+	}
+	@GetMapping("/adProgrammer")
+	public ModelAndView programmerPage(@RequestParam("pId") int id  , @RequestParam("pName") String name, @RequestParam("pLan") String lan,Model model ) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("programmerinfo.html");
+		mv.addObject("id",id);
+		mv.addObject("name",name);
+		mv.addObject("language",lan);
+		return mv;
 	}
 }
