307e9fbe04682c706672325560a0276507d8b932
Mohammad Alamin <alamincsme@gmail.com>
2024-03-25 12:22:42 +0600

    Employee Portal

---
+++ src/main/java/com/alamincsme/EmployeePortalApplication.java
@@ -0,0 +1,20 @@
+package com.alamincsme;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EmployeePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePortalApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
--- src/test/java/io/alamincsme/SpringMvcApplicationTests.java
+++ src/test/java/io/alamincsme/SpringMvcApplicationTests.java
@@ -1,10 +1,10 @@
-package com.alamincsme.springboot;
+package io.alamincsme;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringMvcApplicationTests {
+class EmployeePortalApplicationTests {
 
 	@Test
 	void contextLoads() {
